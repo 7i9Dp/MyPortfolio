2e47ac9531b9f67609f172d8c6a32cc53463d236
Jignesh <jignesh.parmar.201098@gmail.com>
2025-01-05 23:06:07 -0800

    Resposiveness

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  standalone:false
+  standalone: false
 })
 export class HeaderComponent {
 
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const menuElement = document.querySelector('.navbar');
+    const menuIcon = document.getElementById('menu');
+
+    // Close menu if click is outside the menu and menu icon
+    if (menuElement && !menuElement.contains(target) && menuIcon && !menuIcon.contains(target)) {
+      this.menuOpen = false;
+    }
+  }
+  
 }
